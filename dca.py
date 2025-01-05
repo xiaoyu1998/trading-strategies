44b8df5070661151d67d539bb414d651c3ceb859
xiaoyu1998 <1634054493@qq.com>
2025-01-05 15:54:14 +0800

    update place_order and did_fill_order

--- dca.py
+++ dca.py
@@ -13,67 +13,98 @@ from hummingbot.core.event.events import OrderFilledEvent
 from hummingbot.strategy.script_strategy_base import ScriptStrategyBase
 
 
-class SimplePMMConfig(BaseClientModel):
+class Config(BaseClientModel):
     script_file_name: str = Field(default_factory=lambda: os.path.basename(__file__))
     exchange: str = Field("binance_paper_trade", client_data=ClientFieldData(
         prompt_on_new=True, prompt=lambda mi: "Exchange where the bot will trade"))
-    trading_pair: str = Field("ETH-USDT", client_data=ClientFieldData(
+    trading_pair: str = Field("BTC-USDT", client_data=ClientFieldData(
         prompt_on_new=True, prompt=lambda mi: "Trading pair in which the bot will place orders"))
-    order_amount: Decimal = Field(0.01, client_data=ClientFieldData(
-        prompt_on_new=True, prompt=lambda mi: "Order amount (denominated in base asset)"))
-    bid_spread: Decimal = Field(0.001, client_data=ClientFieldData(
-        prompt_on_new=True, prompt=lambda mi: "Bid order spread (in percent)"))
-    ask_spread: Decimal = Field(0.001, client_data=ClientFieldData(
-        prompt_on_new=True, prompt=lambda mi: "Ask order spread (in percent)"))
-    order_refresh_time: int = Field(15, client_data=ClientFieldData(
-        prompt_on_new=True, prompt=lambda mi: "Order refresh time (in seconds)"))
-    price_type: str = Field("mid", client_data=ClientFieldData(
-        prompt_on_new=True, prompt=lambda mi: "Price type to use (mid or last)"))
+    shares: int = Field(1, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "portions for total value in usd"))
+    min_size: float = Field(0.001, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "minimal size of the order"))
+    max_size: float = Field(0.001, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "maximum size of the order"))
+    min_profit_percent: float = Field(0.001, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "minimal profit of the order"))
+    add_position_step_ratio: float = Field(0.02, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "position step increase ratio"))
+    order_delay_time: int = Field(10, client_data=ClientFieldData(
+        prompt_on_new=True, prompt=lambda mi: "Delay time between orders (in seconds)"))
 
 
-class SimplePMM(ScriptStrategyBase):
-    """
-    BotCamp Cohort: Sept 2022
-    Design Template: https://hummingbot-foundation.notion.site/Simple-PMM-63cc765486dd42228d3da0b32537fc92
-    Video: -
-    Description:
-    The bot will place two orders around the price_source (mid price or last traded price) in a trading_pair on
-    exchange, with a distance defined by the ask_spread and bid_spread. Every order_refresh_time in seconds,
-    the bot will cancel and replace the orders.
-    """
+class (ScriptStrategyBase):
 
     create_timestamp = 0
     price_source = PriceType.MidPrice
 
     @classmethod
-    def init_markets(cls, config: SimplePMMConfig):
+    def init_markets(cls, config: Config):
         cls.markets = {config.exchange: {config.trading_pair}}
-        cls.price_source = PriceType.LastTrade if config.price_type == "last" else PriceType.MidPrice
 
-    def __init__(self, connectors: Dict[str, ConnectorBase], config: SimplePMMConfig):
+    def __init__(self, connectors: Dict[str, ConnectorBase], config: Config):
         super().__init__(connectors)
         self.config = config
+        self.token = get_token(self.config.trading_pair)
+        self.user_id = self.config.exchange_user_id
 
     def on_tick(self):
         if self.create_timestamp <= self.current_timestamp:
-            self.cancel_all_orders()
             proposal: List[OrderCandidate] = self.create_proposal()
             proposal_adjusted: List[OrderCandidate] = self.adjust_proposal_to_budget(proposal)
             self.place_orders(proposal_adjusted)
             self.create_timestamp = self.config.order_refresh_time + self.current_timestamp
 
     def create_proposal(self) -> List[OrderCandidate]:
-        ref_price = self.connectors[self.config.exchange].get_price_by_type(self.config.trading_pair, self.price_source)
-        buy_price = ref_price * Decimal(1 - self.config.bid_spread)
-        sell_price = ref_price * Decimal(1 + self.config.ask_spread)
+        usdt_balance = self.connectors[self.config.exchange].get_balance("usdt")
+        token_index_price = self.connectors[self.config.exchange].get_price_by_type(self.config.trading_pair, self.price_source)
+        token_balance = self.connectors[self.config.exchange].get_balance(self.token)
+        total_value_in_usdt = usdt_balance + token_balance * token_index_price
+        unit_size = float(total_value_in_usdt) / self.config.shares
+        if min_size > 0 and unit_size < min_size:
+            unit_size = min_size
+        if max_size > 0 and unit_size > max_size:
+            unit_size = max_size
+        if unit_size < MIN_SPOT_AMOUNT:
+            logger.info(f"#{user_id}:{ex} unit_size: {unit_size:.2f} this unit size is less than exchange minimal amount")
+            return  
 
-        buy_order = OrderCandidate(trading_pair=self.config.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                   order_side=TradeType.BUY, amount=Decimal(self.config.order_amount), price=buy_price)
+        # total = self.connectors[self.config.exchange].get_balance("total")
+        # token_value = token_balance * token_index_price
+        last_price = rdb.get(f"dca:{user_id}:{ex}:{token}:long:price")
+        if not last_price:
+            logger.error(f"#{user_id}:{ex} no last_price for {token}")
+            return
 
-        sell_order = OrderCandidate(trading_pair=self.config.trading_pair, is_maker=True, order_type=OrderType.LIMIT,
-                                    order_side=TradeType.SELL, amount=Decimal(self.config.order_amount), price=sell_price)
+        # increase position
+        ratio = (last_price - token_index_price) / last_price
+        if ratio < -(self.config.add_position_step_ratio):
+            logger.info(f"#{user_id}:{ex} increase position {token}")
+            buy_order = OrderCandidate(
+                trading_pair=self.config.trading_pair, 
+                is_maker=True, 
+                order_type=OrderType.LIMIT,
+                order_side=TradeType.BUY, 
+                amount=Decimal(unit_size), 
+                price=token_index_price
+            )
+            return buy_order
 
-        return [buy_order, sell_order]
+        # decrease position
+        entry_price = rdb.get(f"dca:{user_id}:{ex}:{token}:long:entry_price")
+        ratio = (entry_price - token_index_price) / entry_price
+        if ratio > self.config.add_position_step_ratio
+            logger.info
+            (f"#{user_id}:{ex} increase position {token}")
+            sell_order = OrderCandidate(
+                trading_pair=self.config.trading_pair, 
+                is_maker=True, 
+                order_type=OrderType.LIMIT,
+                order_side=TradeType.SELL, 
+                amount=Decimal(unit_size), 
+                price=token_index_price
+            )
+            return sell_order        
 
     def adjust_proposal_to_budget(self, proposal: List[OrderCandidate]) -> List[OrderCandidate]:
         proposal_adjusted = self.connectors[self.config.exchange].budget_checker.adjust_candidates(proposal, all_or_none=True)
@@ -91,11 +122,22 @@ class SimplePMM(ScriptStrategyBase):
             self.buy(connector_name=connector_name, trading_pair=order.trading_pair, amount=order.amount,
                      order_type=order.order_type, price=order.price)
 
-    def cancel_all_orders(self):
-        for order in self.get_active_orders(connector_name=self.config.exchange):
-            self.cancel(self.config.exchange, order.trading_pair, order.client_order_id)
-
     def did_fill_order(self, event: OrderFilledEvent):
         msg = (f"{event.trade_type.name} {round(event.amount, 2)} {event.trading_pair} {self.config.exchange} at {round(event.price, 2)}")
+
+        entry_price = rdb.get(f"dca:{user_id}:{ex}:{token}:long:entry_price")
+        accumulate_amount = rdb.get(f"dca:{user_id}:{ex}:{token}:long:accumulate_amount")
+        if event.order_type == buy:
+            accumulate_amount += event.amount
+            new_entry_price = (entry_price*accumulate_amount + event.amount*event.price)/ accumulate_amount
+            rdb.set(f"dca:{user_id}:{ex}:{token}:long:entry_price", new_entry_price)
+            rdb.set(f"dca:{user_id}:{ex}:{token}:long:accumulate_amount", accumulate_amount)
+
+        if event.order_type == sell:
+            accumulate_amount -= event.amount
+            rdb.set(f"dca:{user_id}:{ex}:{token}:long:accumulate_amount", accumulate_amount)
+            rdb.set(f"dca:{user_id}:{ex}:{token}:long:entry_price", entry_price)
+
+
         self.log_with_clock(logging.INFO, msg)
         self.notify_hb_app_with_timestamp(msg)
